7bd147625e365a14aba8150b0fc997e4a6f22be1
Logesh <30307881+logeshvnv@users.noreply.github.com>
2017-08-04 17:00:10 +0530

    Create oddinter.c

---
+++ oddinter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+   int num;
+   printf("enter the intervals");
+   printf("Print Odd Numbers in a given range m to n:\n");
+   for (num = m; num <= n; num++)
+   {
+   if (num % 2 == 1)
+   printf ("%d ", num);
+   }
+} 
